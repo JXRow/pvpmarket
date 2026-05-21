a0778892be92a9e793f63666bdd4c68d9707833f
JXRow <85392015@qq.com>
2026-05-21 20:50:27 +0800

    移除test

--- src/model/const.js
+++ src/model/const.js
@@ -12,30 +12,13 @@ export const networks = {
         blockExplorer: 'https://monadscan.com',
         contracts: {
             multicall3: {
-                address: '0xa877a2247b318b40935e102926ba5ff4f3b0e8b1',
+                address: '0xcA11bde05977b3631167028862bE2a173976CA11',
             },
             usdc: {
                 address: '0x754704Bc059F8C67012fEd69BC8A327a5aafb603',
             },
             tradeService: {
                 address: '0x11344C1ebcFd7EEb4d4BAA18A0312aEa854493A7',
-            },
-        },
-    },
-    monadTest: {
-        id: 10143,
-        name: 'Monad Testnet',
-        nativeCurrency: {
-            decimals: 18,
-            name: 'Monad',
-            symbol: 'MON',
-            wrappedToken: '0x3bd359C1119dA7Da1D913D1C4D2B7c461115433A',
-        },
-        rpc: 'https://testnet-rpc.monad.xyz',
-        blockExplorer: 'https://testnet.monadscan.com/',
-        contracts: {
-            multicall3: {
-                address: '0xa877a2247b318b40935e102926ba5ff4f3b0e8b1',
             },
         },
     },
--- src/wagmi.js
+++ src/wagmi.js
@@ -15,26 +15,11 @@ const monad = defineChain({
   contracts: networks.monad.contracts,
 })
 
-const monadTest = defineChain({
-  id: networks.monadTest.id,
-  name: networks.monadTest.name,
-  nativeCurrency: networks.monadTest.nativeCurrency,
-  rpcUrls: {
-    default: { http: [networks.monadTest.rpc] },
-  },
-  blockExplorers: {
-    default: { name: 'Monadscan Testnet', url: networks.monadTest.blockExplorer },
-  },
-  contracts: networks.monadTest.contracts,
-  testnet: true,
-})
-
 export const config = getDefaultConfig({
   appName: 'PvP Market',
   projectId: 'YOUR_WALLETCONNECT_PROJECT_ID',
-  chains: [monad, monadTest],
+  chains: [monad],
   transports: {
     [monad.id]: http(),
-    [monadTest.id]: http(),
   },
 })
